98bbdb3df1d56a774c4b207f0b16727c23e9e01d
hank <anhui3713@vip.qq.com>
2019-08-28 23:52:22 +0800

    init

---
+++ src/test/java/space/maganda/iossign/superiosign/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package space.maganda.iossign.superiosign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
